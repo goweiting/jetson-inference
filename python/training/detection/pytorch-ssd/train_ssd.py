1d1fb0a411334aee141a8566c1c040474404c9b1
gwt <gwt.543@gmail.com>
2020-09-13 20:38:15 +0800

    move torch wrapper outside the for loop

--- python/training/detection/pytorch-ssd/train_ssd.py
+++ python/training/detection/pytorch-ssd/train_ssd.py
@@ -105,7 +105,6 @@ if args.use_cuda and torch.cuda.is_available():
     torch.backends.cudnn.benchmark = True
     logging.info("Using CUDA...")
 
-
 def train(loader, net, criterion, optimizer, device, debug_steps=100, epoch=-1):
     net.train(True)
     running_loss = 0.0
@@ -147,21 +146,25 @@ def test(loader, net, criterion, device):
     running_regression_loss = 0.0
     running_classification_loss = 0.0
     num = 0
-    for _, data in enumerate(loader):
-        images, boxes, labels = data
-        images = images.to(device)
-        boxes = boxes.to(device)
-        labels = labels.to(device)
-        num += 1
 
-        with torch.no_grad():
+
+    with torch.no_grad(): # diable gradient calculation
+        for _, data in enumerate(loader):
+            images, boxes, labels = data
+            images = images.to(device)
+            boxes = boxes.to(device)
+            labels = labels.to(device)
+            num += 1
+
+
             confidence, locations = net(images)
             regression_loss, classification_loss = criterion(confidence, locations, labels, boxes)
             loss = regression_loss + classification_loss
 
-        running_loss += loss.item()
-        running_regression_loss += regression_loss.item()
-        running_classification_loss += classification_loss.item()
+            running_loss += loss.item()
+            running_regression_loss += regression_loss.item()
+            running_classification_loss += classification_loss.item()
+
     return running_loss / num, running_regression_loss / num, running_classification_loss / num
 
 
@@ -208,10 +211,7 @@ if __name__ == '__main__':
     # load datasets (could be multiple)
     logging.info("Prepare training datasets.")
     datasets = []
-
     # for dataset_path in args.datasets:
-    dataset_path = args.datasets[0]
-
     if args.dataset_type == 'voc':
         dataset = VOCDataset(dataset_path, transform=train_transform,
                              target_transform=target_transform)
@@ -243,9 +243,9 @@ if __name__ == '__main__':
 
     # create training dataset
     logging.info(f"Stored labels into file {label_file}.")
-    #train_dataset = ConcatDataset(datasets)
-    logging.info("Train dataset size: {}".format(len(dataset)))
-    train_loader = DataLoader(dataset,
+    train_dataset = ConcatDataset(datasets)
+    logging.info("Train dataset size: {}".format(len(train_dataset)))
+    train_loader = DataLoader(train_dataset,
                               batch_size=args.batch_size,
                               num_workers=args.num_workers,
                               shuffle=True)
@@ -266,7 +266,7 @@ if __name__ == '__main__':
     logging.info(val_dataset)
     logging.info("Validation dataset size: {}".format(len(val_dataset)))
 
-    val_loader = DataLoader(val_dataset, 2, # args.batch_size,
+    val_loader = DataLoader(val_dataset, args.batch_size,
                             num_workers=args.num_workers,
                             shuffle=False)
 
@@ -356,12 +356,11 @@ if __name__ == '__main__':
     logging.info(f"Start training from epoch {last_epoch + 1}.")
 
     for epoch in range(last_epoch + 1, args.num_epochs):
+
         train(train_loader, net, criterion, optimizer,
               device=DEVICE, debug_steps=args.debug_steps, epoch=epoch)
 
         if epoch % args.validation_epochs == 0 or epoch == args.num_epochs - 1:
-            logging.info("loading validation set")
-
             val_loss, val_regression_loss, val_classification_loss = test(val_loader, net, criterion, DEVICE)
             logging.info(
                 f"Epoch: {epoch}, " +
